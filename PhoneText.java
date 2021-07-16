6d28e080b69ac3581553a2b9097a3b4626929876
macong0503 <1512346520@qq.com>
2021-07-16 16:05:07 +0800

    Create PhoneText.java

---
+++ PhoneText.java
@@ -0,0 +1,15 @@
+package com.hqyj.mc.Text;
+
+public class PhoneText {
+    public static void main(String[] args) {
+       Phone phone = new Phone();
+//        System.out.println(phone.brand);
+//        System.out.println(phone.colour);
+//        System.out.println(phone.price);
+//        phone.call();
+//        phone.sms();
+        phone.price=998;
+          phone.callphone();
+
+    }
+}
