42a19575fc22fe8769528c81bc35c672fe766cd4
macong0503 <1512346520@qq.com>
2021-07-16 16:04:54 +0800

    Create Cat.java

---
+++ Cat.java
@@ -0,0 +1,22 @@
+package com.hqyj.mc.Text;
+/*
+* 类的创建：
+*    权限修饰符（默认 public） class 类名{
+* 类名
+*    符号规则，没有要求非要大写，但是约定要大写
+* */
+public class Cat {
+        //属性：成员变量
+    String name = "HelloKitty";
+    int age = 5;
+
+         //行为：成员方法
+    //跑方法
+    public void run(){
+        System.out.println("我会跑");
+    }
+    //睡觉
+    public void sleep(){
+        System.out.println("我睡觉了");
+    }
+}
